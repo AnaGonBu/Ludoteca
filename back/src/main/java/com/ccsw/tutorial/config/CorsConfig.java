fa90276205f01b31eadb9ff1fb7a9b6a7793a103
Ana González Bueno <anagbueno@gmail.com>
2025-10-17 12:27:25 +0200

    nuevo cors

--- back/src/main/java/com/ccsw/tutorial/config/CorsConfig.java
+++ back/src/main/java/com/ccsw/tutorial/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
