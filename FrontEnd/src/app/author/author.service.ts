b44459c7c948dfe4ee2c7263937e56f1a2b18619
Ana González Bueno <anagbueno@gmail.com>
2025-10-17 11:31:57 +0200

    configure production environment and fix animations dependency

--- FrontEnd/src/app/author/author.service.ts
+++ FrontEnd/src/app/author/author.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environment/environment';
+import { environment } from '../../environments/environment';
 import { Pageable } from '../core/model/page/Pageable';
 import { Author } from './model/Author';
 import { AuthorPage } from './model/AuthorPage';
